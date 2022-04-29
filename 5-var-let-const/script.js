51b2d4d3ac658578c52f0b639922d9670aff63f5
muhammad rafi cahyaputra <71932553+Synnefo15@users.noreply.github.com>
2022-04-29 10:55:25 +0700

    const-let-var

---
+++ 5-var-let-const/script.js
@@ -0,0 +1,13 @@
+const rafi = function(){
+    console.log(`fungsi rafi`);
+}
+
+function gilang() {
+    console.log(`Fungsi gilang`);
+}
+
+const syta = () => {
+    console.log(`fungsi syta`);
+}
+
+rafi(),gilang(),syta()
